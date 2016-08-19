25cdcfb21632d475e281fb0cdea703ee8363a936
Animesh Sinha <AnimeshSinha1309@gmail.com>
2016-08-19 19:17:49 +0530

    Hello USART?
    The first working USART program, simple one bite transfer, ery basic but works.

--- MicrocontrollerSystem/main.cpp
+++ MicrocontrollerSystem/main.cpp
@@ -1,19 +1,26 @@
 #include <avr/io.h>
-#include "drivers/USART.h"
 #include <util/delay.h>
+#include "drivers/USART.h"
 #include "drivers/LED.h"
+
+#define CODE_TXD // Currently programming the transmitter
 
 using namespace Integral;
 
 int main(void)
 {
-	_delay_ms(2000);
 	USART usart(960);
 	LED led(IOPINA0);
-	usart.transmit(0xff);
-//	if (usart.receive()==0xff)
-	led.on();
+	#ifdef CODE_TXD
+		_delay_ms(2000);
+		usart.transmit(0xd3);
+		led.on();
+	#endif
 	while (1)
-	{	
+	{
+		#ifdef CODE_RXD
+			if (usart.receive() == 0xd3)
+				led.on();
+		#endif
 	}
 }
